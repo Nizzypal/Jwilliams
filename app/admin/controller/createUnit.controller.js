c1621733722794f35cf57a7a68045977b64c37c8
unknown <jvillegas@KMCS4DJY132.kmc.local>
2015-04-15 19:20:38 +0800

    Create Unit w/ test controller

---
+++ app/admin/controller/createUnit.controller.js
@@ -0,0 +1,61 @@
+angular.module().controller('CreateUnitCtrl', function($scope, $http, API_URL){
+
+  var unit = {
+		name: "",
+		type: "",
+		floor: "",
+		floorCount: "",
+		price: "",
+		size: "",
+		bedroomCount: "",
+		bathroomCount: "",
+		quartersCount: "",
+		powderCount: "",
+		forShortTerm: false,
+		forLongTerm: false,
+		forSale: true,
+		condominiumName: "",
+		city: "",
+		address: "",
+		photos: []
+  };
+  //get reference of variable to scope to bind it
+  $scope.unit = unit;
+
+  var rentInfo = {
+		monthlyRate: 0,
+		dailyRate: 0,
+		blockDateStart: "",
+		blockDateEnd: "",
+		blockDates: [],
+		currentRenter: "",
+		numberMonthsAdvance: 0,
+		numberMonthsDeposit: 0,
+		cancellationFee: 0,
+		terminationFee: 0,
+		includeUtilities: false,
+		includeInternet: false,
+		requirePassport: false,
+		requireAlienCard: false,
+		requireID: false,
+		unitAmenities: [],
+		buildingAmenities: []
+  };
+  //get reference of variable to scope to bind it
+  $scope.rentInfo = rentInfo;
+
+  var createUnitData = {
+  	unit: unit,
+  	renInfo: rentInfo
+  }
+  $scope.unitCreate = function(){
+  	alert( "Handler for .unitCreate() called." );
+
+  	$http.post(API_URL + 'createUnit', createUnitData)
+  		.success(function(){})
+  		.error(function(err){
+  			alert('warning: ' + err.message);
+  		});
+  };
+
+})
