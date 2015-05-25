67f02a7c39874617239f65d635500ed2421720aa
Nizzypal <nazarite_paladin@yahoo.com>
2015-05-25 15:19:03 +0800

    interim

--- app/scripts/controller/login.controller.js
+++ app/scripts/controller/login.controller.js
@@ -1,8 +1,29 @@
-angular.module('jwilliams').controller('LoginCtrl', function($scope, $stateParams){
-	vm = this;
+'use strict';
+angular.module('jwilliams').controller('LoginCtrl', function($scope, $stateParams, $http, API_URL){
+	var vm = this;
+
+	var user = {
+		email:'',
+		password:''
+	};
+
+	$scope.user = user;
+	$scope.email = user.email;
+	$scope.password = user.password;
+
 	vm.submit = function(){
-		alert('test');
+		alert('user - ' + user.email);
+
+	    $http.post(API_URL + 'login', user)
+	        .success(function(newInquiry){
+	            //inquiryID = newInquiry.newInquiryID;
+	        })
+	        .error(function(err){
+	            alert('warning: ' + err.message);
+	            return false;
+	        });		
 	};
 
 	$scope.submit = vm.submit;
+
 });
